b7fcea1fdd5c09c8fa8f978c1f29cec9c8118d62
smartrector <smartrector@gmail.com>
2024-05-08 11:38:01 +0900

    write v1

--- front/src/pages/PostWrite.js
+++ front/src/pages/PostWrite.js
@@ -1,7 +1,109 @@
-import React from "react";
+import React, {useState} from "react";
+const continents = [
+  {key: 1, value: "seoul"},
+  {key: 2, value: "pusan"},
+  {key: 3, value: "gangwon"},
+  {key: 4, value: "suwon"},
+  {key: 5, value: "daegu"},
+];
 
 function PostWrite() {
-  return <div>PostWrite</div>;
+  const [product, setProduct] = useState({
+    title: "",
+    discription: "",
+    price: 0,
+    continents: 1,
+    images: [],
+  });
+
+  function handelChange(e) {
+    const {name, value} = e.target;
+    console.log(value, name);
+    setProduct((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  }
+
+  function handelSubmit(e) {
+    e.preventDefault();
+    alert("입력");
+  }
+
+  return (
+    <section>
+      <h2>자료입력</h2>
+
+      <form onSubmit={handelSubmit}>
+        <div className="mb-4">
+          <label htmlFor="title" className="block mb-3">
+            제목
+          </label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            className="w-full px-4 py-2 border rounded-md"
+            onChange={handelChange}
+            value={product.title}
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="discription" className="block mb-3">
+            설명
+          </label>
+          <input
+            type="text"
+            id="discription"
+            name="discription"
+            className="w-full px-4 py-2 border rounded-md"
+            onChange={handelChange}
+            value={product.discription}
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="price" className="block mb-3">
+            가격
+          </label>
+          <input
+            type="text"
+            id="price"
+            name="price"
+            className="w-full px-4 py-2 border rounded-md"
+            onChange={handelChange}
+            value={product.price}
+          />
+        </div>
+        <div className="mb-4">
+          <label htmlFor="continents" className="block mb-3">
+            지역
+          </label>
+          <select
+            className="w-full px-4 py-2 border rounded-md"
+            name="continents"
+            id="continents"
+            onChange={handelChange}
+            value={product.continents}
+          >
+            {continents.map((item) => {
+              return (
+                <option value={item.key} key={item.key}>
+                  {item.value}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div>
+          <button className="w-full px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-700">
+            글작성완료
+          </button>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default PostWrite;
