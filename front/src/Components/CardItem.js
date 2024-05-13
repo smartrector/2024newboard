87e797fe47a2b652d915cca1e4ce96aeb8f9cd52
smartrector <smartrector@gmail.com>
2024-05-13 15:11:54 +0900

    file up v5

--- front/src/Components/CardItem.js
+++ front/src/Components/CardItem.js
@@ -1,7 +1,36 @@
 import React from "react";
+import styled from "styled-components";
 
 function CardItem({product}) {
-  return <div className="border py-4 px-2">{product.title}</div>;
+  const ImageWrap = styled.div`
+    width: 100%;
+    height: 140px;
+    overflow: hidden;
+    &:hover img {
+      // div:hover img{}
+      transform: scale(1.2);
+    }
+    img {
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
+      transition: 0.3s;
+    }
+  `;
+  return (
+    <div className="border py-4 px-2">
+      {product.images.length > 0 && (
+        <ImageWrap>
+          <img
+            src={`${process.env.REACT_APP_NODE_SERVER_URL}/uploads/${product.images[0]}`}
+            alt=""
+          />
+        </ImageWrap>
+      )}
+
+      <div>{product.title}</div>
+    </div>
+  );
 }
 
 export default CardItem;
