eb94ed41b6e0365670c53559287c4ae06b35b755
smartrector <smartrector@gmail.com>
2024-05-13 13:24:45 +0900

    upload v4

--- front/src/Components/FileUpload.js
+++ front/src/Components/FileUpload.js
@@ -15,15 +15,21 @@ function FileUpload({images, onImageChange}) {
     try {
       const res = await axiosInstance.post("/products/image", formData, config);
       console.log(res.data);
-      alert("upok");
+      onImageChange([...images, res.data]);
     } catch (error) {
       console.log(error);
     }
   }
+  function handleDelete(image) {
+    const currentIndex = images.indexOf(image);
+    let newImages = [...images];
+    newImages.splice(currentIndex, 1);
+    onImageChange(newImages);
+  }
   return (
     <div>
       <h2>file</h2>
-      <div>
+      <div className="flex gap-4">
         <div className="w-[100px] h-[100px] flex justify-center items-center bg-blue-300 rounded-md overflow-hidden">
           <Dropzone onDrop={handleDrop}>
             {({getRootProps, getInputProps}) => (
@@ -35,6 +41,26 @@ function FileUpload({images, onImageChange}) {
               </div>
             )}
           </Dropzone>
+        </div>
+        <div className="flex items-center gap-4">
+          {images.map((image) => {
+            return (
+              <div key={image} className="w-[90px] relative ">
+                <div
+                  onClick={() => {
+                    handleDelete(image);
+                  }}
+                  className="w-[20px] h-[20px] flex justify-center items-center bg-red-300 absolute rounded-[50%] right-[-10px] top-[-10px]"
+                >
+                  X
+                </div>
+                <img
+                  src={`${process.env.REACT_APP_NODE_SERVER_URL}/uploads/${image}`}
+                  alt=""
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
